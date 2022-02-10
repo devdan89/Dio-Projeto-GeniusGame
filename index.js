3644eb6a86390afd7973fc33953b43a6ae95deb9
Dan <danilobomfim.si@gmail.com>
2022-02-09 21:02:05 -0300

    Done.

--- index.js
+++ index.js
@@ -1,50 +1,108 @@
-var order = [];
+let order = [];
 let orderUser = [];
-
-//0 - green
-//1 - red
-//2 - yellow
-//3 - blue
+let score = 0;
 
 const green = document.querySelector(".green");
 const red = document.querySelector(".red");
 const yellow = document.querySelector(".yellow");
 const blue = document.querySelector(".blue");
 
+/*
+0 - green
+1 - red
+2 - yellow
+3 - blue
+ Array contendo o nome das cores em seus respectivos indices. */
 let colors = [green, red, yellow, blue];
 
-let colorChange = (color, number) => {
-  if (!color.classList.contains("selected")) {
-    order[order.length] = number;
-    setTimeout(() => {
-      color.classList.add("selected");
-      console.log(color.classList.contains("selected"));
-      console.log("Adicionou");
-    }, 1000);
+// Função para gerar cor aleatoria.
+let randomGenerator = () => {
+  let colorOrder = Math.floor(Math.random() * 4);
+  order[order.length] = colorOrder;
+  orderUser = [];
 
-    setTimeout(() => {
-      console.log(color.classList.contains("selected"));
-      color.classList.remove("selected");
-      console.log("Removeu");
-    }, 3000);
-
-    console.log("O numero sorteado foi: ",order[order.length-1] )
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
   }
 };
 
-let randomColor = () => {
-  let rng = null;
+//acendendo a cor sorteada
+let lightColor = (element, number) => {
+  number = number * 500;
   setTimeout(() => {
-    for (let i in colors) {
-      rng = Math.floor(Math.random() * colors.length);
-      console.log(rng);
-      console.log("Random Color: ", i);
-      colorChange(colors[i], rng);
-    }
-  }, 10000);
+    element.classList.add("selected");
+  }, number) - 250;
+  setTimeout(() => {
+    element.classList.remove("selected");
+  });
 };
 
-randomColor();
+//Verificando se a ordem do usuario é a mesma sorteada.
+let checkOrder = () => {
+  for (let i in orderUser) {
+    if (orderUser[i] != order[i]) {
+      gameOver();
+      break;
+    }
+  }
+  if (orderUser.length == order.length) {
+    alert(`Score: ${score}\nVocê acertou! Iniciando próximo nível!`);
+    nextLevel();
+  }
+};
 
+//Função de click
+let click = (color) => {
+  orderUser[orderUser.length] = color;
+  createColorElement(color).classList.add("selected");
 
+  setTimeout(() => {
+    createColorElement(color).classList.remove("selected");
+    checkOrder();
+  }, 250);
+};
 
+//Alterando a coloração para destaque.
+let createColorElement = (color) => {
+  for (let i in colors) {
+    if (color == i) {
+      return colors[i];
+    }
+  }
+};
+
+//Controle de dificuldade.
+let nextLevel = () => {
+  score++;
+
+  randomGenerator();
+};
+
+//Função para finalizar o jogo em caso de derrota.
+let gameOver = () => {
+  alert(
+    `Score: ${score}!\n Você perdeu o jogo!\nClique em OK para iniciar um novo jogo`
+  );
+  order = [];
+  orderUser = [];
+
+  playGame();
+};
+
+//Iniciando o jogo
+let playGame = () => {
+  score = 0;
+  alert("Bem vindo ao Genius!\n Iniciando novo jogo!");
+
+  nextLevel();
+};
+
+//Conferindo o clique do usuario.
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+//Chamando a função de inicio do jogo.
+playGame();
